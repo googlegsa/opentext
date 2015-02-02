9b831a128a8fa8e40068787387b4cd67b91005d2
Gennis Emerson <gennis.d.emerson@gmail.com>
2015-02-02 11:22:06 -0800

    Make initial connection to Content Server.
    Add the required WSDL file for authentication. Update build file
    to generate Java code for web services from WSDL. Read
    configuration properties and use to authenticate with Content
    Server using web services.
    
    http://codereview.appspot.com/196470043

--- src/com/google/enterprise/adaptor/opentext/OpentextAdaptor.java
+++ src/com/google/enterprise/adaptor/opentext/OpentextAdaptor.java
@@ -19,22 +19,86 @@ import com.google.enterprise.adaptor.AdaptorContext;
 import com.google.enterprise.adaptor.Config;
 import com.google.enterprise.adaptor.DocId;
 import com.google.enterprise.adaptor.DocIdPusher;
+import com.google.enterprise.adaptor.InvalidConfigurationException;
 import com.google.enterprise.adaptor.Request;
 import com.google.enterprise.adaptor.Response;
 
+import com.opentext.livelink.service.core.Authentication;
+import com.opentext.livelink.service.core.Authentication_Service;
+
+import java.net.URL;
+import java.net.UnknownHostException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.xml.soap.SOAPFault;
+import javax.xml.ws.BindingProvider;
+import javax.xml.ws.WebServiceException;
+import javax.xml.ws.soap.SOAPFaultException;
+
 /** For getting OpenText repository content into a Google Search Appliance. */
 public class OpentextAdaptor extends AbstractAdaptor {
+
+  private static final Logger log
+      = Logger.getLogger(OpentextAdaptor.class.getName());
 
   public static void main(String[] args) {
     AbstractAdaptor.main(new OpentextAdaptor(), args);
   }
 
-  @Override
-  public void initConfig(Config config) {
-  }
+  /** The Authentication service object. */
+  private Authentication authentication;
 
   @Override
-  public void init(AdaptorContext context) throws Exception {
+  public void initConfig(Config config) {
+    config.addKey("opentext.webServicesUrl", null);
+    config.addKey("opentext.username", null);
+    config.addKey("opentext.password", null);
+  }
+
+  /**
+   * Verifies the configured Content Web Services location and
+   * credentials.
+   *
+   * @throws InvalidConfigurationException if the hostname or
+   * credentials are invalid
+   * @throws SOAPFaultException if the Content Server is unavailable
+   * @throws WebServiceException if the adaptor can't connect to
+   * the web services server
+   */
+  @Override
+  public void init(AdaptorContext context) {
+    Config config = context.getConfig();
+
+    String webServicesUrl = config.getValue("opentext.webServicesUrl");
+    String username = config.getValue("opentext.username");
+    String password = context.getSensitiveValueDecoder().decodeValue(
+        config.getValue("opentext.password"));
+    log.log(Level.CONFIG, "opentext.webServicesUrl: {0}", webServicesUrl);
+    log.log(Level.CONFIG, "opentext.username: {0}", username);
+
+    Authentication_Service authService = new Authentication_Service(
+        Authentication_Service.class.getResource("Authentication.wsdl"));
+    Authentication authPort = authService.getBasicHttpBindingAuthentication();
+    ((BindingProvider) authPort).getRequestContext().put(
+        BindingProvider.ENDPOINT_ADDRESS_PROPERTY,
+        webServicesUrl + "/Authentication");
+    try {
+      authPort.authenticateUser(username, password);
+    } catch (SOAPFaultException soapFaultException) {
+      SOAPFault fault = soapFaultException.getFault();
+      String localPart = fault.getFaultCodeAsQName().getLocalPart();
+      if ("Core.LoginFailed".equals(localPart)) {
+        throw new InvalidConfigurationException(fault.getFaultString(),
+            soapFaultException);
+      }
+      // The only other currently known exception code here is
+      // Core.ServiceException, seen when the Content Server was
+      // unavailable. We want to allow the adaptor to retry if
+      // that's the error.
+      throw soapFaultException;
+    }
+    this.authentication = authPort;
   }
 
   @Override
@@ -46,4 +110,3 @@ public class OpentextAdaptor extends AbstractAdaptor {
   public void getDocContent(Request req, Response resp) {
   }
 }
-
